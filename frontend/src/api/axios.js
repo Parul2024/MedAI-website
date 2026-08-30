551b562fecf4208fe32654534ff3c7365f103975
Parul2024 <parul11th@gmail.com>
2026-08-30 13:05:18 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://med-ai-website-pxjy.vercel.app/",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("medai_token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
@@ -16,10 +20,39 @@ api.interceptors.response.use(
     if (err.response?.status === 401) {
       localStorage.removeItem("medai_token");
       localStorage.removeItem("medai_user");
-      if (window.location.pathname !== "/login") window.location.href = "/login";
+
+      if (window.location.pathname !== "/login") {
+        window.location.href = "/login";
+      }
     }
+
     return Promise.reject(err);
   }
 );
 
 export default api;
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "/api",
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("medai_token");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// api.interceptors.response.use(
+//   (res) => res,
+//   (err) => {
+//     if (err.response?.status === 401) {
+//       localStorage.removeItem("medai_token");
+//       localStorage.removeItem("medai_user");
+//       if (window.location.pathname !== "/login") window.location.href = "/login";
+//     }
+//     return Promise.reject(err);
+//   }
+// );
+
+// export default api;
